3e1337ace0bf02455bed271ea249dbeb7ad9d8c1
Robert Relyea <robert.relyea@gmail.com>
2026-02-07 19:51:55 -0800

    Multiple HX711 sensors read

--- main.c
+++ main.c
@@ -7,37 +7,57 @@
 
 int main() {
     setup_default_uart();
-    printf("Hello, world!\n");
-    
+
+    // Set up multiple HX711 sensors
     printf("Configuring HX711\n");
-    // Set up single HX711 sensor
 
-    hx711_config_t hxcfg;
-    hx711_get_default_config(&hxcfg);
+    // 1. Set configuration
+    hx711_multi_config_t hxmcfg;
+    hx711_multi_get_default_config(&hxmcfg);
+    hxmcfg.clock_pin = 9; //GPIO pin connected to each HX711 chip
+    hxmcfg.data_pin_base = 14; //first GPIO pin used to connect to HX711 data pin
+    hxmcfg.chips_len = 2; //how many HX711 chips connected
 
-    hxcfg.clock_pin = 14;
-    hxcfg.data_pin = 15;
+    //by default, the underlying PIO programs will run on pio0
+    //if you need to change this, you can do:
+    //hxmcfg.pio = pio1;
 
-    hx711_t hx;
+    // 2. Initialise
+    hx711_multi_t hxm;
+    hx711_multi_init(&hxm, &hxmcfg);
 
-    printf("Initializing HX711\n");
-    hx711_init(&hx, &hxcfg);
-    printf("Powering up HX711\n");
-    hx711_power_up(&hx, hx711_gain_128);
+    // 3. Power up the HX711 chips and set gain on each chip
+    hx711_multi_power_up(&hxm, hx711_gain_128);
 
-    printf("Settling HX711\n");
-    hx711_wait_settle(hx711_rate_80);
+    // 4. This step is optional. Only do this if you want to
+    // change the gain AND save it to each HX711 chip
+    //
+    // hx711_multi_set_gain(&hxm, hx711_gain_64);
+    // hx711_multi_power_down(&hxm);
+    // hx711_wait_power_down();
+    // hx711_multi_power_up(&hxm, hx711_gain_64);
 
-    int32_t val = 0;
+    // 5. Wait for readings to settle
+    hx711_wait_settle(hx711_gain_128);
 
-    printf("Reading from HX711\n");
+    // 6. Read values
+    int32_t arr[hxmcfg.chips_len];
+
+
     while(true)
     {
-      if(hx711_get_value_noblock(&hx, &val))
-	{
-	  printf("value: %li\n", val);
-	}
+      // 6a. wait (block) until values are ready
+      hx711_multi_get_values(&hxm, arr);
+
+      // then print the value from each chip
+      // the first value in the array is from the HX711
+      // connected to the first configured data pin and
+      // so on
+      for(uint i = 0; i < hxmcfg.chips_len; ++i) {
+	printf("hx711_multi_t chip %i: %li\n", i, arr[i]);
+      }
     }
+
         
     return 0;
 }
